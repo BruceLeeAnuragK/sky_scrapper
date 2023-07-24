23c81137fff2e82ca74d8850ffc179bca4408060
BruceLeeK <kanadeanurag87@gmail.com>
2023-07-24 06:44:29 +0530

    Initial commit

---
+++ lib/view/weather_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Weather App",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
